88b0a475a03bae5fa2e4216fd18bd14a8a5730fa
贾丽鹏 <1094425279@qq.com>
2024-02-26 16:40:38 +0800

    feat #mini-vue代码

--- mini-vue.js
+++ mini-vue.js
@@ -17,7 +17,8 @@
       this.initMethods()
       // 监听数据
       new Observer(this._data)
-
+      // 解析指令
+      new Compile(this)
       this.initWatch()
 
     },
@@ -51,7 +52,6 @@
       }
     },
 
-
     proxy(target, sourceKey, key) {
       const sharedPropertyDefinition = {
         enumerable: true,
@@ -70,8 +70,6 @@
     $watch(variable, callback) {
       new Watcher(this, variable, callback)
     }
-
-
   }
 
   // 对数据进行监听
@@ -89,7 +87,34 @@
   }
 
   function defineReactive(obj, key, val) {
-
+    const dep = new Dep()
+    // 如果值是一个对象 递归监听
+    if (typeof val === 'object') {
+      new Observer(val)
+    }
+    Object.defineProperty(obj, key, {
+      enumerable: true,
+      configurable: true,
+      get() {
+        // 收集对应的观察者对象
+        if (Dep.target) {
+          dep.depend()
+        }
+        return val
+      },
+      set(newVal) {
+        if (val === newVal) {
+          return
+        }
+        val = newVal
+        // 如果新值是对象 递归监听
+        if (typeof val === 'object') {
+          new Observer(val)
+        }
+        // 触发更新
+        dep.notify()
+      }
+    })
   }
 
   // watcher实例的ID 每个watcher实现的ID都是唯一的
@@ -123,9 +148,34 @@
       // 触发依赖后置空
       Dep.target = null
       return value
+    },
+    set(val) {
+      this.setter(val)
+    },
+    update() {
+      this.run()
+    },
+    run() {
+      // 触发更新后执行回调函数
+      const value = this.get()
+      const oldValue = this.value
+      if (value !== oldValue) {
+        this.cb.call(this.vm, value, oldValue)
+      }
+      this.value = value
+    },
+    addDep(dep) {
+      // 触发依赖 dep添加观察者对象 同时观察者对象也会将dep实例添加到自己的deps里
+      // 如果dep已经存在在deps里，则不添加
+      // dep中存放着对应的watcher watcher中也会存放着对应的dep
+      // 一个dep可能有多个watcher 一个watcher也可能对应着多个dep
+      if (!this.depIds.has(dep.id)) {
+        this.deps.push(dep)
+        this.depIds.add(dep.id)
+        dep.addSub(this)
+      }
     }
   }
-
 
   // dep实例的ID
   let did = 0
@@ -142,8 +192,173 @@
       if (Dep.target) {
         Dep.target.addDep(this)
       }
+    },
+
+    addSub(sub) {
+      this.subs.push(sub)
+    },
+
+    removeSub(sub) {
+      const index = this.subs.indexOf(sub)
+      if (index > -1) {
+        this.subs.splice(index, 1)
+      }
+    },
+
+    notify() {
+      this.subs.forEach(e => {
+        e.update()
+      })
+    }
+  }
+
+  // 针对各种指令的回调函数
+  const handles = {
+    on: {
+      implement(vm, el, name, expOrFn) {
+        el['on' + name] = vm[expOrFn].bind(vm)
+      },
+      update(vm, el, expOrFn, newVal, oldVal) {
+
+      }
+    },
+    bind: {
+      implement(vm, el, name, expOrFn) {
+        el.setAttribute(expOrFn, vm[expOrFn])
+      },
+      update(vm, el, expOrFn, newVal, oldVal) {
+        el.setAttribute(expOrFn, newVal)
+      }
+    },
+    model: {
+      implement(vm, el, name, expOrFn) {
+        el.value = vm[expOrFn]
+        el.oninput = function () {
+          vm[expOrFn] = this.value
+        }
+      },
+      update(vm, el, expOrFn, newVal, oldVal) {
+        el.value = newVal
+      }
+    },
+    textNode: {
+      implement(vm, textNode, variable) {
+        textNode.nodeValue = textNode.nodeValue.replace(`{{${variable}}}`, vm[variable])
+      },
+      update(vm, newVal, oldVal, textNode, variable, rawValue, re1, re2) {
+        textNode.nodeValue = rawValue.replace(`{{${variable}}}`, newVal)
+        let str = textNode.nodeValue
+        if (re1.test(str)) {
+          let arry = str.match(re1)
+          arry.forEach(e => {
+            let variable = e.replace(re2, '')
+            str = str.replace(e, vm[variable])
+          })
+          textNode.nodeValue = str
+        }
+      }
     }
   }
 
 
+  // 指令解析器
+  function Compile(vm) {
+    this.el = vm.$el
+    this.vm = vm
+    this.onRe = /^(v-on:|@)/
+    this.modelRe = /^v-model/
+    this.bindRe = /^(v-bind:|:)/
+    this.braceRe1 = /{{\w+}}/g
+    this.braceRe2 = /[{}]/g
+    this.dirs = []
+    this.handles = handles
+    this.init()
+  }
+
+  Compile.prototype = {
+    init() {
+      this.parse(this.el)
+      this.render()
+    },
+    parse(el) {
+      const attrs = el.attributes
+      let name
+      [...attrs].forEach(e => {
+        if (this.onRe.test(e.name)) {
+          name = e.name.replace(this.onRe, '')
+          this.addDir(this.handles.on, name, e.name, e.value, el)
+        } else if (this.bindRe.test(e.name)) {
+          // 类似:bind="name" 解析完后将原本的值删掉
+          el.removeAttribute(e.name.split('=')[0])
+          name = e.name.replace(this.bindRe, '')
+          this.addDir(this.handles.bind, name, e.name, e.value, el)
+        } else if (this.modelRe.test(e.name)) {
+          name = e.name.replace(this.modelRe, '')
+          this.addDir(this.handles.model, name, e.name, e.value, el)
+        }
+      })
+
+      const children = el.childNodes
+      if (children.length > 0) {
+        children.forEach(ele => {
+          switch (ele.nodeType) {
+            // 元素节点
+            case 1:
+              this.parse(ele)
+              break
+            // 文本节点
+            case 3:
+              if (this.braceRe1.test(ele.nodeValue)) {
+                this.vm._textNodes.push(ele)
+              }
+              break
+          }
+        })
+      }
+    },
+
+    addDir(handle, dirName, name, value, el) {
+      this.dirs.push({
+        vm: this.vm,
+        dirName,
+        handle,
+        rawName: name,
+        expOrFn: value,
+        el
+      })
+    },
+
+    render() {
+      const vm = this.vm
+      const that = this
+      this.dirs.forEach(e => {
+        const handle = e.handle
+        if (handle.implement) {
+          handle.implement(e.vm, e.el, e.dirName, e.expOrFn)
+        }
+        const update = function (newVal, oldVal) {
+          handle.update(e.vm, e.el, e.expOrFn, newVal, oldVal)
+        }
+        // 在这里开始创建观察者实例 将监听的值变化时 触发update回调函数
+        new Watcher(this.vm, e.expOrFn, update)
+      })
+      const handles = this.handles.textNode
+
+      vm._textNodes.forEach(e => {
+        let arry = e.nodeValue.match(this.braceRe1)
+        let rawValue = e.nodeValue
+        arry.forEach(str => {
+          let variable = str.replace(this.braceRe2, '')
+          handles.implement(vm, e, variable)
+          const update = function (newVal, oldVal) {
+            handles.update(vm, newVal, oldVal, e, variable, rawValue, that.braceRe1, that.braceRe2)
+          }
+          // 监听文本节点 在这里开始创建观察者实例 将监听的值变化时 触发update回调函数
+          new Watcher(vm, variable, update)
+        })
+      })
+    }
+  }
+  // 将minivue添加到window上
+  root.MiniVue = MiniVue
 })(window)
